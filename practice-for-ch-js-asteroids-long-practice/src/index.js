10bfdbc80402f7abd566185c4c4595e7a50c3de7
Matthew Montejo <montejododger@yahoo.com>
2023-05-22 14:47:27 -0700

    started draw method

--- practice-for-ch-js-asteroids-long-practice/src/index.js
+++ practice-for-ch-js-asteroids-long-practice/src/index.js
@@ -1,3 +1,13 @@
-console.log("Webpack is working!");
-import MovingObject from "./moving_object";
-window.MovingObject = MovingObject;
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasEl = document.getElementById("canvas");
+    // canvasEl.width = 500;
+    // canvasEl.height = 700;
+
+    const ctx = canvasEl.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 500, 700);
+});
+
+    console.log("Webpack is working!");
+    import MovingObject from "./moving_object";
+    window.MovingObject = MovingObject;
--- practice-for-ch-js-asteroids-long-practice/src/moving_object.js
+++ practice-for-ch-js-asteroids-long-practice/src/moving_object.js
@@ -1,12 +1,17 @@
 class MovingObject {
     constructor(options) {
-    [pos, vel, radius, color] = options;
+        const { pos, vel, radius, color } = options;
 
         this.pos = pos;
         this.vel = vel;
         this.radius = radius;
         this.color = color;
     }
-};
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(pos[0], pos[1], radius, 0, 2 * Math.Pi, true);
+    }
+}
 
 export default MovingObject;
